1f77baf4a2e963da15fec356ce1de6e42e2035ee
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2020-12-11 16:29:51 +0100

    Add PostController@show action

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,10 @@ class PostController extends Controller
         $posts = Post::all();
         return view('posts.index', compact('posts'));
     }
+
+    public function show($slug)
+    {
+        $post = Post::where('slug', $slug)->first();
+        return view('posts.show', compact('post'));
+    }
 }
